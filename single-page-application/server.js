95e9511837cbaebc90fe5613c3ae354061e41d89
Perry <chenzhipeng901105@gmail.com>
2018-07-23 00:32:47 +0800

    add code

--- single-page-application/server.js
+++ single-page-application/server.js
@@ -1,2 +1,15 @@
 require('.dotenv').config(); // read .env files
 const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000
+
+// Set public folder as root
+app.use(express.static('public'));
+
+// Allow front-end access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.listen(port, () => {
+    console.log('listening on %d', port);
+})
